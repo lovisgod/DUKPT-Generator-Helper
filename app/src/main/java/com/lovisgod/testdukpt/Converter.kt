367766df18be7dedfe8e2ce935f5136560804bc4
lovisgod <ayooluwa.olosunde@gmail.com>
2024-03-20 08:50:16 +0100

    feature:tested dukpt

---
+++ app/src/main/java/com/lovisgod/testdukpt/Converter.kt
@@ -0,0 +1,20 @@
+package com.lovisgod.testdukpt
+
+object Converter {
+    val keyUtils = KeyUtils()
+
+    /**
+     * @param ksn is the KSN with the Key counter
+     * the key counter must increment per transaction
+     * most times it's the same value with the STAN
+     * @param ipek is gotten from your provider
+     * @param plainpin is the pin for provided by the card holder
+     * @param pan is the pan of the card being read by the terminal
+     * */
+    fun GetPinBlock(ipek: String, ksn: String, plainpin: String, pan: String): String {
+        val workingKey = keyUtils.getWorkingKey(IPEK = ipek, KSN = ksn)
+        val pinBlock = keyUtils.DesEncryptDukpt( workingKey = workingKey, pan = pan, clearPin = plainpin )
+        println("****************The expected value of the pinblock is: $pinBlock")
+        return pinBlock
+    }
+}
---
+++ app/src/main/java/com/lovisgod/testdukpt/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.lovisgod.testdukpt
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<TextView>(R.id.testbtn).setOnClickListener {
+            checkLogic()
+        }
+    }
+
+    private fun checkLogic() {
+        val block  = Converter.GetPinBlock("3F2216D8297BCE9C",
+            "0000000006DDDDE00001", "1994", "4105400015916727")
+
+        println("this is the pin block oo => $block")
+
+    }
+}
